5c5da040a25b690daeaf9b62a54a7a12c82b68c4
Bruce Kim <mannypac12@gmail.com>
2018-12-25 13:30:00 +0900

    Til Handling Purchases

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,44 @@
+import * as actionTypes from './actionTypes'
+import axios from '../../axios-orders'
+
+
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT, 
+        ingredientName: name
+    } 
+};
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    }
+};
+
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS, 
+        ingredients: ingredients
+    }
+}
+
+export const fetchIngredientFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    }
+}
+
+export const initIngredients = () => {
+
+    return dispatch => {
+        axios.get('https://burgerbuilder-b4f1d.firebaseio.com/ingredients.json')
+             .then (res => {
+                 dispatch(setIngredients(res.data))
+             })
+             .catch( error => {
+                 dispatch(fetchIngredientFailed())
+             })
+    }
+
+}
