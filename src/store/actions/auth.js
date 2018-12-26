f6badbf4f41795a601f41edc0ed43ef22a52c99e
Bruce Kim <mannypac12@gmail.com>
2018-12-26 21:57:03 +0900

    Til Adding a Spinner

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     }
 }
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionType.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     }
 }
 
@@ -22,22 +23,28 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password, 
             returnSecureToken: true
+        }; 
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCvKfmxQcXH6qHBUo3KyBn7K8FofqtZ7eY'
+
+        if (!isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCvKfmxQcXH6qHBUo3KyBn7K8FofqtZ7eY'
         }
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCvKfmxQcXH6qHBUo3KyBn7K8FofqtZ7eY', authData)
+        axios.post(url, authData)
              .then(res => {
                  console.log(res);
-                 dispatch(authSuccess(res.data))
+                 dispatch(authSuccess(res.data.idToken, 
+                                      res.data.localId))
              })
              .catch(err => {
                  console.log(err);
-                 dispatch(authFail(err))
+                 dispatch(authFail(err.response.data.error))
              });
     }
 }
